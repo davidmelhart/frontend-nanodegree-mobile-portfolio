3efd418a4acd8d45e03acde404a15e0235e5b48f
David Melhart <david.melhart@gmail.com>
2015-04-27 12:25:55 +0200

    Version 1.2

--- views/js/main.js
+++ views/js/main.js
@@ -541,9 +541,10 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 21; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    // Resized and compressed pizza.png to reduce rendering time.
+    elem.src = "images/pizza-bg.png";
     elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    elem.style.width = "100px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
